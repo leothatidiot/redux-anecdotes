187fbc7db77c0aa9ba6c9f864a17f40aa015b2db
Leo <m.leo.cen@outlook.com>
2022-10-15 20:08:37 +0800

    feat: 6.9

--- src/components/AnecdoteList.js
+++ src/components/AnecdoteList.js
@@ -3,7 +3,7 @@ import { voteTo } from "../reducers/anecdoteReducer";
 
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => state.anecdote)
   const dispatch = useDispatch()
 
   const vote = (id) => {
