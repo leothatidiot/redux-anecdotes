d83e9ce0eec24607879986561b26fea6b7c3080f
Leo <m.leo.cen@outlook.com>
2022-11-23 22:23:23 +0800

    feat: 6.17

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -5,31 +5,42 @@ const anecdoteSlice = createSlice({
   name: 'anecdote',
   initialState: [],
   reducers:{
-    voteTo(state, action) {
-      const id = action.payload
-      const anecToChange = state.find(anec => anec.id === id)
-      const changedAnec = {
-        ...anecToChange, votes: anecToChange.votes + 1
-      }
-      return state.map(anec =>
-        anec.id !== id ? anec : changedAnec
-      ).sort((a, b) => {
-        if (a.votes < b.votes) return 1
-        else if (a.votes > b.votes) return -1
-        else return 0
-      })
-    },
+  //   voteTo(state, action) {
+  //     const id = action.payload
+  //     const anecToChange = state.find(anec => anec.id === id)
+  //     const changedAnec = {
+  //       ...anecToChange, votes: anecToChange.votes + 1
+  //     }
+  //     return state.map(anec =>
+  //       anec.id !== id ? anec : changedAnec
+  //     ).sort((a, b) => {
+  //       if (a.votes < b.votes) return 1
+  //       else if (a.votes > b.votes) return -1
+  //       else return 0
+  //     })
+  //   },
     appendAnecdote(state, action) {
       state.push(action.payload)
     },
     setAnecdotes(state, action) {
       const newState = action.payload
       return newState
+    },
+    setAnecdote(state, action) {
+      const id = action.payload.id
+      const changedAnec = action.payload
+      return state.map(anec => 
+        anec.id === id ? changedAnec : anec
+      ).sort((a, b) => { // 点赞后排序 仅在 store 中排序 不在后端数据库排序
+        if (a.votes < b.votes) return 1
+        else if (a.votes > b.votes) return -1
+        else return 0
+      })
     }
   }
 })
 
-export const { voteTo, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
+export const { appendAnecdote, setAnecdotes, setAnecdote } = anecdoteSlice.actions
 
 export const initializeAnecdotes = () => {
   return async (dispatch) => {
@@ -49,6 +60,15 @@ export const createAnecdote = (content) => {
     }
     const CreateAnecRes = await anecdoteService.createNew(newAnecObject)
     dispatch(appendAnecdote(CreateAnecRes))
+  }
+}
+
+export const voteTo = (id) => {
+  return async (dispatch, getState) => {
+    const anecToVote = getState().anecdotes.find(anec => anec.id === id)
+    const votedAnec = { ...anecToVote, votes: anecToVote.votes+1 }
+    const updateAnecRes = await anecdoteService.update(id, votedAnec)
+    dispatch(setAnecdote(updateAnecRes))
   }
 }
 
--- src/services/anecdotes.js
+++ src/services/anecdotes.js
@@ -12,5 +12,10 @@ const createNew = async (object) => {
   return response.data
 }
 
+const update = async (objectId, objectJSON) => {
+  const response = await axios.put(`${baseUrl}/${objectId}`, objectJSON)
+  return response.data
+}
+
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { getAll, createNew }
+export default { getAll, createNew, update }
